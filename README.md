c989ff58ff5e19c0fb90c76d02aa91c34a2090f8
unknown <joaodiasconde@gmail.com>
2020-07-31 01:41:37 +0100

    More opcodes implemented but videomem always set to zero

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ### References
 1. https://en.wikipedia.org/wiki/CHIP-8
-2. http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#0.1
+2. http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
+3. http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
+4. https://github.com/hugoferreira/chip8
+5. https://github.com/loktar00/chip8/
+6. https://github.com/stianeklund/chip8
--- src/main.ts
+++ src/main.ts
@@ -20,10 +20,10 @@ const testRom: number[] = [
     0x6b, 0x20, 0xdb, 0xa1, 0x00, 0xee
 ]
 
-type Chip8State = {
+type Chip8Type = {
     videoMem: Uint8Array,
     mem: Uint8Array,
-    vRegisters: Int8Array,
+    vRegisters: Uint8Array,
     I: number,
     delay: number,
     sound: number,
@@ -41,63 +41,73 @@ const image = ctx.createImageData(canvas.width, canvas.height)
 const videoBuff = new DataView(image.data.buffer); //interface to manipulate pixels (R,G,B,A format i.e. 4 bytes or 32 bits)
 //
 
-
-
-//DRAW
-// for (let i = 0; i < canvas.width * canvas.height; i++) { //TODO: remove
-//     chip8.videoMem[i] = Math.round(Math.random());
-// }
-// //
-
-
-function render(chip8: Chip8State){
+function render(chip8: Chip8Type){
     for (let i = 0, j = 0; i < chip8.videoMem.length; i++, j += 4) {
         videoBuff.setUint32(j, chip8.videoMem[i] === 1 ? PIXEL_SET_COLOR : PIXEL_UNSET_COLOR);
     }
     ctx.putImageData(image, 0, 0)
 }
 
-function execute(chip8: Chip8State){
-    const instruction = rom[chip8.pc] << 8 | rom[chip8.pc+1]
-    const id = instruction & 0xF000
-    const addr = instruction & 0x0FFF
-    const nibble = instruction & 0x000F
-    const x = instruction & 0x0F00
-    const y = instruction & 0x00F0
-    const byte = instruction & 0x00FF
+function drawSprite(chip8: Chip8Type, x0: number, y0: number, height: number){
+    let collision = 0
     
+    for (let y = 0; y < height; y += 1) {
+        const sprite = chip8.mem[chip8.I + y]
+        for (let x = 0; x < 8; x += 1) {
+            if((sprite & (0x80 >> x)) != 0){
+                if(chip8.videoMem[x0 + x + ((y0 + y) * 640)] == 1)
+                    collision = 1
+                chip8.videoMem[x0 + x + ((y0 + y) * 640)] ^= 1
+            }
+        } 
+    }
+
+    return collision
+}
+
+function execute(chip8: Chip8Type){
+    const opcode = chip8.mem[chip8.pc] << 8 | chip8.mem[chip8.pc+1]
+    const id = opcode & 0xF000
+    const addr = opcode & 0x0FFF
+    const nibble = opcode & 0x000F
+    const x = opcode & 0x0F00
+    const y = opcode & 0x00F0
+    const byte = opcode & 0x00FF
+    
+    chip8.pc += 2 // take two opcodes from ROM
     switch(id){
         case 0x0000:
             switch(byte){
                 case 0xE0:
-                    // clear screen
+                    //chip8.videoMem.map(_ => 0)
                     break
                 case 0xEE:
-                    chip8.pc = chip8.stack[chip8.sp]
                     chip8.sp--
+                    chip8.pc = chip8.stack[chip8.sp]
                     break
             }    
             break
         
         case 0x1000:
+            chip8.pc = addr
             break
         
         case 0x2000:
-            chip8.sp++
             chip8.stack[chip8.sp] = chip8.pc
+            chip8.sp++
             chip8.pc = addr
             break
 
         case 0x3000:
-            if(chip8.vRegisters[x] == byte) chip8.pc += 3 //TODO is it 2 or 3?
+            if(chip8.vRegisters[x] == byte) chip8.pc += 2
             break
         
         case 0x4000:
-            if(chip8.vRegisters[x] != byte) chip8.pc += 3 //TODO is it 2 or 3?
+            if(chip8.vRegisters[x] != byte) chip8.pc += 2
             break
         
         case 0x5000:
-            if(chip8.vRegisters[x] == chip8.vRegisters[y]) chip8.pc += 3 //TODO is it 2 or 3?
+            if(chip8.vRegisters[x] == chip8.vRegisters[y]) chip8.pc += 2
             break
         
         case 0x6000:
@@ -105,14 +115,48 @@ function execute(chip8: Chip8State){
             break
 
         case 0x7000:
-            chip8.vRegisters[x] = chip8.vRegisters[x] + byte
+            chip8.vRegisters[x] = chip8.vRegisters[x] + byte //TODO what if it overflows?
             break
         
         case 0x8000:
+            switch(nibble){
+                case 0x0:
+                    chip8.vRegisters[x] = chip8.vRegisters[y]
+                    break
+                case 0x1:
+                    chip8.vRegisters[x] |= chip8.vRegisters[y]
+                    break
+                case 0x2:
+                    chip8.vRegisters[x] &= chip8.vRegisters[y]
+                    break
+                case 0x3:
+                    chip8.vRegisters[x] ^= chip8.vRegisters[y]
+                    break
+                case 0x4:
+                    chip8.vRegisters[0xF] = chip8.vRegisters[x] + chip8.vRegisters[y] > 0xFF ? 1 : 0
+                    chip8.vRegisters[x] = chip8.vRegisters[x] + chip8.vRegisters[y]
+                    break
+                case 0x5:
+                    chip8.vRegisters[x] = chip8.vRegisters[x] - chip8.vRegisters[y]
+                    //TODO VF = borrow
+                    break
+                case 0x6:
+                    chip8.vRegisters[x] = chip8.vRegisters[x] >> 1
+                    //TODO VF = ?
+                    break
+                case 0x7:
+                    chip8.vRegisters[x] = chip8.vRegisters[y] - chip8.vRegisters[x]
+                    //TODO VF = borrow
+                    break
+                case 0xE:
+                    chip8.vRegisters[x] = chip8.vRegisters[x] << 1
+                    //TODO VF = ?
+                    break
+            }
             break
 
         case 0x9000:
-            if(chip8.vRegisters[x] != chip8.vRegisters[y]) chip8.pc += 3 //TODO is it 2 or 3?
+            if(chip8.vRegisters[x] != chip8.vRegisters[y]) chip8.pc += 2
             break
         
         case 0xA000:
@@ -124,11 +168,11 @@ function execute(chip8: Chip8State){
             break
 
         case 0xC000:
-            chip8.vRegisters[x] = byte & Math.floor(Math.random() * 255)
+            chip8.vRegisters[x] = byte & Math.floor(Math.random() * 0xFF)
             break
         
         case 0xD000:
-            //TODO needs elaborate logic
+            chip8.vRegisters[0xF] = drawSprite(chip8, chip8.vRegisters[x], chip8.vRegisters[y], nibble)
             break
 
         case 0xE000:
@@ -136,45 +180,105 @@ function execute(chip8: Chip8State){
             break
         
         case 0xF000:
-
+            switch(byte){
+                case 0x07:
+                    chip8.vRegisters[x] = chip8.delay
+                    break
+                case 0x0A:
+                    // TODO execution must stop
+                    break
+                case 0x15:
+                    chip8.delay = chip8.vRegisters[x]
+                    break
+                case 0x18:
+                    chip8.sound = chip8.vRegisters[x]
+                    break
+                case 0x1E:
+                    chip8.I = chip8.I + chip8.vRegisters[x]
+                    break
+                case 0x29:
+                    // view stuff requires sprite map
+                    break
+                case 0x33:
+                    chip8.mem[chip8.I] = Math.floor(chip8.vRegisters[x] / 100)
+                    chip8.mem[chip8.I+1] = Math.floor(chip8.vRegisters[x] / 10) % 10
+                    chip8.mem[chip8.I+2] = chip8.vRegisters[x] % 10
+                    break
+                case 0x55:
+                    for(let i = 0; i <= x; i++){
+                        chip8.mem[chip8.I + i] = chip8.vRegisters[i]
+                    }
+                    break
+                case 0x65:
+                    for(let i = 0; i <= x; i++){
+                        chip8.vRegisters[i] = chip8.mem[chip8.I + i]
+                    }
+                    break
+            }
             break
 
         default:
             console.log("Panic, instruction not recognized!")
     }
 
-    chip8.pc += 2
+    if(chip8.delay > 0) chip8.delay--
+
+    if(chip8.sound > 0) {
+        if(chip8.sound)
+            console.log("Beep!")
+        chip8.sound--
+    }
 }
 
 document.getElementById("playBtn").onclick = function(e) {
-    play()
+    loadROM()
 }
 
-let rom = null
-document.getElementById("romInput").onchange = function(e) {
-    const file = (e.target as HTMLInputElement).files[0]
+function loadROM(){
+    const romInput = document.getElementById("romInput") as HTMLInputElement
     const fr = new FileReader()
-    fr.onload = () => {rom = new Uint8Array(fr.result as ArrayBuffer)}
-    fr.readAsArrayBuffer(file)
+    fr.onload = () => {const rom = new Uint8Array(fr.result as ArrayBuffer); play(rom)}
+    fr.readAsArrayBuffer(romInput.files[0])
 }
 
-function play(){
+function play(rom){
+    
     rom = testRom;
 
-    const chip8: Chip8State = {
+    const chip8: Chip8Type = {
         videoMem: new Uint8Array(canvas.width * canvas.height), //active and not active bits (0s and 1s)
         mem: new Uint8Array(4096),
-        vRegisters: new Int8Array(16),
-        I: 0x0000,
-        delay: 0x00,
-        sound: 0x00,
-        pc: 0x0000,
-        sp: 0x00,
+        vRegisters: new Uint8Array(16),
+        I: 0,
+        delay: 0,
+        sound: 0,
+        pc: 0x200,
+        sp: 0,
         stack: new Int16Array(16)
     }
     
-    for(let i = 0; i < 294; i++){
-        execute(chip8)
-        render(chip8)
-    }
+    const fontSet = [ 
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    ];
+
+    chip8.mem.set(fontSet, 0x00) // load font
+    chip8.mem.set(rom, 0x200) // load rom into memory
+
+    window.setInterval(() => {execute(chip8); render(chip8);}, 100)
+    window.setInterval(() => {console.log(chip8.videoMem)}, 3000)
 }
